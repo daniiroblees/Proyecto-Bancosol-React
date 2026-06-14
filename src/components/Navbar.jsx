8d996cba890d4d73ba5abf0f61452c8985d5f343
Danieh <148767868+daniiroblees@users.noreply.github.com>
2026-06-14 14:01:19 +0200

    Added security to the navbar and applaoyut

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, Outlet } from 'react-router-dom';
 import '../styles/navbar.css';
 import logo from '/src/assets/LOGO_BANCOSOL.png';
 
@@ -37,58 +37,63 @@ export default function Navbar() {
 
 
     return (
-        <div className="main-header">
-            <div className="top-navbar">
-                <div className="logo-container">
-                    <img src={logo} alt="Logo Bancosol" className="logo-img" />
+            <>
+            <div className="main-header">
+                <div className="top-navbar">
+                    <div className="logo-container">
+                        <img src={logo} alt="Logo Bancosol" className="logo-img" />
+                    </div>
                 </div>
-            </div>
-            
-            <nav className="bottom-navbar">
-                <ul className="nav-menu">
-                    <li className={checkActive('/campanyas')}>
-                        <Link to="/campanyas" className="nav-link">
-                            <i className="ri-megaphone-line"></i>
-                            <span>Gestión de Campañas</span>
-                        </Link>
-                    </li>
-                    
-                    <li className={checkActive('/tiendas')}>
-                        <Link to="/tiendas" className="nav-link">
-                            <i className="ri-store-2-line"></i>
-                            <span>Tiendas</span>
-                        </Link>
-                    </li>
-                    
-                    <li className={checkActive('/colaboradores')}>
-                        <Link to="/colaboradores" className="nav-link">
-                            <i className="ri-user-heart-line"></i>
-                            <span>Colaboradores</span>
-                        </Link>
-                    </li>
-                    
-                    <li className={checkActive('/coordinadores')}>
-                        <Link to="/coordinadores" className="nav-link">
-                            <i className="ri-team-line"></i>
-                            <span>Coordinadores</span>
-                        </Link>
-                    </li>
-                    
-                    <li className={checkActive('/asignacion_turno')}>
-                        <Link to="/asignacion_turno" className="nav-link">
-                            <i className="ri-clipboard-line"></i>
-                            <span>Asignación de turno</span>
-                        </Link>
-                    </li>
-                </ul>
+                
+                <nav className="bottom-navbar">
+                    <ul className="nav-menu">
+                        <li className={checkActive('/campanyas')}>
+                            <Link to="/campanyas" className="nav-link">
+                                <i className="ri-megaphone-line"></i>
+                                <span>Gestión de Campañas</span>
+                            </Link>
+                        </li>
+                        
+                        <li className={checkActive('/tiendas')}>
+                            <Link to="/tiendas" className="nav-link">
+                                <i className="ri-store-2-line"></i>
+                                <span>Tiendas</span>
+                            </Link>
+                        </li>
+                        
+                        <li className={checkActive('/colaboradores')}>
+                            <Link to="/colaboradores" className="nav-link">
+                                <i className="ri-user-heart-line"></i>
+                                <span>Colaboradores</span>
+                            </Link>
+                        </li>
+                        
+                        <li className={checkActive('/coordinadores')}>
+                            <Link to="/coordinadores" className="nav-link">
+                                <i className="ri-team-line"></i>
+                                <span>Coordinadores</span>
+                            </Link>
+                        </li>
+                        
+                        <li className={checkActive('/asignacion_turno')}>
+                            <Link to="/asignacion_turno" className="nav-link">
+                                <i className="ri-clipboard-line"></i>
+                                <span>Asignación de turno</span>
+                            </Link>
+                        </li>
+                    </ul>
 
-                <div className="logout-section">
-                    <a href="#" className="logout-link" onClick={handleLogout}>
-                        <i className="ri-logout-box-r-line"></i>
-                        <span>Cerrar Sesión</span>
-                    </a>
-                </div>
-            </nav>
-        </div>
+                    <div className="logout-section">
+                        <a href="#" className="logout-link" onClick={handleLogout}>
+                            <i className="ri-logout-box-r-line"></i>
+                            <span>Cerrar Sesión</span>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+            <div className="page-content" style={{ padding: '20px' }}>
+                    <Outlet /> 
+            </div>
+        </>
     );
 }
